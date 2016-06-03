7286fe027de9cafe7db022019dec5301e3b124b6
Newlog <newlog@overflowedminds.net>
2016-06-02 19:01:42 -0700

    Modify file

--- test.cs
+++ test.cs
@@ -1,8 +1,1 @@
-// Compose a string that consists of three lines.
-string lines = "First line.\r\nSecond line.\r\nThird line.";
-
-// Write the string to a file.
-System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\test.txt");
-file.WriteLine(lines);
-
-file.Close();
+echo test > c:\test.txt
