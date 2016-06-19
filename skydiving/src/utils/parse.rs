24fe62beb8fc37d4d9f6fbd24ea5629088f8ecc0
newlog <newlog@overflowedminds.net>
2016-06-18 20:54:21 -0700

    Remove initial // and add http schema if not present

--- skydiving/src/utils/parse.rs
+++ skydiving/src/utils/parse.rs
@@ -5,7 +5,11 @@ pub fn get_image_links(html: &str) -> Vec<String> {
     let mut v: Vec<String> = vec![];
     let document = Document::from(html);
     for node in document.find(Name("img")).iter() {
-        let link = node.attr("src").unwrap().to_owned();
+        let mut link = node.attr("src").unwrap().to_owned();
+        link = link.trim_left_matches("/").to_owned();
+        if !link.starts_with("http://") && !link.starts_with("https://") {
+            link = "http://".to_owned() + &link;
+        }
         if !v.contains(&link) {
             v.push(link);
         }
