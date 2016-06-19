18e75b65d76723bedf71ab10861f1a802d2958e3
newlog <newlog@overflowedminds.net>
2016-06-18 20:31:46 -0700

    Remove limit on number of images downloaded

--- skydiving/src/main.rs
+++ skydiving/src/main.rs
@@ -4,17 +4,12 @@ extern crate select;
 mod utils;
 
 fn download_images(image_links: Vec<String>) {
-    let mut count = 0u32;
     for image_link in image_links {
         let image_name = utils::parse::get_name_from_image_link(&image_link);
         let mut response = utils::http::get_response(&image_link);
         let mut file = utils::file::create_file(&image_name);
         let bytes_written = utils::file::write_response_to_file(&mut file, &mut response);
         println!("{} saved. Size: {} bytes", image_name, bytes_written);
-        if count ==  1 {
-            break;
-        }         
-        count += 1;  
     }
 }
 
